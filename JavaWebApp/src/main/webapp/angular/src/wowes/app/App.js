4699055076e8a7b19811e3de7057fb77e359dbf7
wendel <wfleming@usc.edu>
2015-10-02 06:03:53 -0700

    Code cleanup

--- JavaWebApp/src/main/webapp/angular/src/wowes/app/App.js
+++ JavaWebApp/src/main/webapp/angular/src/wowes/app/App.js
@@ -1,10 +1,3 @@
-//var app = angular.module('WoWES', []);
-//
-//app.controller('WoWESCtrl', WoWESCtrl);
-//
-//function WoWESCtrl($scope) {
-//    $scope.greeting = new Date();
-//}
 
 
 angular.module('WoWES.Common', ['ui.bootstrap', 'WoWES.Search']);
