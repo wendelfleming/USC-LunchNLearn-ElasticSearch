630c79bc44f850d22af12e92afc0a33a31881f71
Wendel Fleming <wfleming@usc.edu>
2015-10-03 06:14:33 -0700

    Made the local to container URL work.

--- JavaWebApp/src/main/webapp/angular/src/wowes/app/services/ConfigService.js
+++ JavaWebApp/src/main/webapp/angular/src/wowes/app/services/ConfigService.js
@@ -1,9 +1,9 @@
 
 angular.module('WoWES.confservices', [])
     .factory('ConfigService', function() {
-        var localRestServerURL = "http://localhost:8080/elasticwow/spring/";
+        var localRestServerURL = "/elasticwow/spring/";
         var remoteRestServerURL = "http://192.168.59.103:8080/elasticwow/spring/";
-        var useLocal = true;
+        var useLocal = false;
 
         return {
             getRestServerURL: function() { return useLocal ? localRestServerURL : remoteRestServerURL; }
